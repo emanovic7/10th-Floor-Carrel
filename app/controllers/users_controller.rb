0799a1fae45418e542b9f8851475809f3ff130f7
Emmanuel Owusu <github email address>
2018-03-28 01:45:45 -0400

    build post/register route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
     erb :'/users/register'
   end
 
+  post '/register' do
+    if params[:user_name] == "" || params[:email] == "" || params[:password_digest] == ""
+      redirect to '/register'
+    else
+      @user=User.create(user_name: params[:user_name], email: params[:email], password: params[:password_digest])
+      session[:user_id] = @user.id
+      redirect '/users/home_page'
+    end
+  end
+
+
+
 end
