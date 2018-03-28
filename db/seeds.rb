76d6a87bd32ce53fbbba8573ca287296bc68477b
Emmanuel Owusu <github email address>
2018-03-27 21:48:08 -0400

    add seeds file to db folder

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+naasei = User.create(user_name: "Naasei", email:"e_owusu@live.com")
+anna = User.create(user_name: "Anna", email:"a_hobler@live.com")
+jp = User.create(user_name: "Jonathan", email:"jp@live.com")
+dustin = User.create(user_name: "Dustin", email:"dtaylor@live.com")
+abby = User.create(user_name: "Abigale", email:"ahoke@live.com")
+sean = User.create(user_name: "Sean", email:"pearson@live.com")
+vlad = User.create(user_name: "Vladimir", email:"vlad@live.com")
+madeline = User.create(user_name: "Madeline", email:"moleary@live.com")
