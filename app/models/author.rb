d74f92b12af511bed5850481ba67bb70dde599ad
Emmanuel Owusu <github email address>
2018-03-27 21:14:31 -0400

    add bcrypt to gemfile

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ActiveRecord::Base
 
   has_many :books
-  has_many :subjects through: :books
+  has_many :users, through: :books
+  has_many :subjects, through: :books
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  
+
   has_many :books
+  has_many :authors, through: :books
 
 end
