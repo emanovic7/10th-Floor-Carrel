96f8eaa47221c6e7dd2c294b6b4a639eb65e7838
Emmanuel Owusu <github email address>
2018-03-27 20:52:54 -0400

    define associations between models

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ActiveRecord::Base
 
-
+  has_many :books
+  has_many :subjects through: :books
 
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,5 @@
 class Subject < ActiveRecord::Base
 
+  belongs_to :books
 
 end
