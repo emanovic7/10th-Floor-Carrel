199531cda53403424ad1abf1c1e1f68dbf3bbef6
Emmanuel Owusu <github email address>
2018-03-31 17:33:05 -0400

    add '/authors' to authors_controller, create authors view folder

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,13 @@
 class AuthorsController < ApplicationController
 
+  get '/authors' do
+    if logged_in?
+      @authors = Author.all
+      erb :'/authors/authors'
+    else
+      redirect to '/login'
+    end
+  end
+
 
 end
